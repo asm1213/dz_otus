c6a12cf6ce3b5053fab9cceb54123da2c3c4c79d
srg <serg@vodc.ru>
2021-09-30 16:18:46 +0300

    nwq

--- DZ_21/openssl.sh
+++ DZ_21/openssl.sh
@@ -1,9 +1,10 @@
+#gen CA
+openssl genrsa -out ca.key 4096
+openssl req -new -x509 -days 365 -key ca.key -out ca.crt
+#gen srv 
 openssl genrsa -out server.key 4096
-   37  openssl req -new -key server.key -out server.csr
-   38  openssl x509 -req -days 365  -CA ca.crt -CAkey ca.key -set_serial 01 -extfile openssl-csr.cnf -extensions req_ext -in server.csr -out server.crt
-   39  openssl x509 -req -days 365  -CA ca.crt -CAkey ca.key -set_serial 01 -extensions req_ext -in server.csr -out server.crt
-   40  ls
-   41  vi server.crt
-   42  openssl dhparam -out dhparams.pem 2048
-   43  ls
-   44  history
+openssl req -new -key server.key -out server.csr
+openssl x509 -req -days 365  -CA ca.crt -CAkey ca.key -set_serial 01 -extfile openssl-csr.cnf -extensions req_ext -in server.csr -out server.crt
+openssl x509 -req -days 365  -CA ca.crt -CAkey ca.key -set_serial 01 -extensions req_ext -in server.csr -out server.crt
+openssl dhparam -out dhparams.pem 2048
+ 
