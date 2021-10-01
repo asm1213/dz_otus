dbc64090e0bdfa0aac3ca9e2457d40c90af53e11
srg <serg@vodc.ru>
2021-10-01 14:40:26 +0300

    last

--- DZ_21/openssl.sh
+++ DZ_21/openssl.sh
@@ -4,7 +4,11 @@ openssl req -new -x509 -days 365 -key ca.key -out ca.crt
 #gen srv 
 openssl genrsa -out server.key 4096
 openssl req -new -key server.key -out server.csr
-openssl x509 -req -days 365  -CA ca.crt -CAkey ca.key -set_serial 01 -extfile openssl-csr.cnf -extensions req_ext -in server.csr -out server.crt
 openssl x509 -req -days 365  -CA ca.crt -CAkey ca.key -set_serial 01 -extensions req_ext -in server.csr -out server.crt
+#gen client 
+openssl genrsa -out client.key 4096
+openssl req -new -key client.key -out client.csr
+openssl x509 -req -days 365  -CA ca.crt -CAkey ca.key -set_serial 01 -extensions req_ext -in client.csr -out client.crt
+#Diffie hellman pem
 openssl dhparam -out dhparams.pem 2048
  
