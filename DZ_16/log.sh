272ba7b8f868f916d2417e9fa1f4c2ef04cc9b84
srg <serg@vodc.ru>
2021-10-25 10:54:48 +0300

    dz_16_new

--- DZ_16/log.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-yum -y update
-yum -y install systemd-journal-gateway
-mkdir -p /var/log/journal/remote
-chown systemd-journal-remote:systemd-journal-remote /var/log/journal/remote
-systemctl start 
